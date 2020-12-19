e9c852f7ab49f826649007461767ea36026199f5
Allan <allan.sduarte@gmail.com>
2020-12-19 16:28:26 -0300

    Add generators

--- lib/eqc_gen.ex
+++ lib/eqc_gen.ex
@@ -16,4 +16,36 @@ defmodule EQCGen do
       vector(len, frequency([{3, oneof(:lists.seq(?a, ?z))}, {1, ?,}]))
     end
   end
+
+  def balanced_tree(gen) do
+    sized size do
+      balanced_tree(size, gen)
+    end
+  end
+
+  def balanced_tree(0, gen) do
+    {:leaf, gen}
+  end
+
+  def balanced_tree(n, gen) do
+    lazy do
+      {:node, gen, balanced_tree(div(n, 2), gen), balanced_tree(div(n, 2), gen)}
+    end
+  end
+
+  def nested_list(gen) do
+    sized size do
+      nested_list(size, gen)
+    end
+  end
+
+  defp nested_list(0, _gen) do
+    []
+  end
+
+  defp nested_list(n, gen) do
+    lazy do
+      oneof([[gen | nested_list(n - 1, gen)], [nested_list(n - 1, gen)]])
+    end
+  end
 end
